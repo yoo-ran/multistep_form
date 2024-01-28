040f50619377d365e47415826e543dc1a08305f0
YooranKim <yuranm@naver.com>
2024-01-28 14:32:26 -0800

    Design

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import RadioGroup from '@mui/material/RadioGroup';
 import Radio from '@mui/material/Radio';
-import FormControl from '@mui/material/FormControl';
+// import FormControl from '@mui/material/FormControl';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Container from '@mui/material/Container';
 
 import CssBaseline from '@mui/material/CssBaseline';
-import {makeStyles } from '@mui/styles';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { lime, purple } from '@mui/material/colors';
+// import {makeStyles } from '@mui/styles';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import { lime, purple } from '@mui/material/colors';
 
 
 
@@ -46,24 +46,58 @@ function App() {
   };
 
 
-  const theme = createTheme({
-    palette: {
-      primary: lime,
-      secondary: purple,
-    },
-    Container:{
-      border:"1px solid red",
+  const containerStyle = {
+    display:"flex",
+    flexDirection:"column",
+    justifyContent:"center",
+    alignItems: 'center',
+    height:"100vh",
+    width:"100vw",
+    backgroundColor:"whitesmoke",
+  };
 
-    }
-  });
-  
+  const boxStyle = {
+    border:"1px solid red",
+    display:"flex",
+    flexDirection:"column",
+    justifyContent:"center",
+    alignItems: 'center',
+    rowGap: '10vh',
+    width:"70%",
+    height:"70vh",
+    minHeight:"60vh"
+  }
 
-  const container = createTheme({
-    palette:{
+  const formStyle = {
+    display:"flex",
+    flexDirection:"column",
+    height:"20vh"
+    // other styles
+  };
+  const inputStyle = {
+    display:"flex",
+    flexWrap:"wrap",
+    justifyContent:"center",
+    alignItems: 'center',
+    columnGap:"20px"
+  };
+
+  const inputWidth={
+    width:"45%"
+  }
+
+  const reviewStyle = {
       display:"flex",
-      border:"1px solid red"
-    }
-  });
+      flexWrap:"wrap",
+      justifyContent:"space-between",
+      alignItems:"center",
+      rowGap:"5rem"
+  }
+
+  const reviewBox ={
+    border:"1px solid red",
+    width:"30%"
+  }
   
 
   const stepContent = (step) => {
@@ -71,32 +105,31 @@ function App() {
 
     switch (step) {
       case 1:
-        return <Box>
+        return <Box style={formStyle}>
             <Typography >Personal Information</Typography>
             <Typography>Please provide your personal details so we can get to know you better</Typography>
-            <Box>
-            <TextField label="Full Name" variant="standard" onChange={(event)=>{setName(event.target.value)}}/>          
-            <TextField label="Email Address" variant="standard" onChange={(event)=>{setEmail(event.target.value)}} />          
-            <TextField label="Phone Number" variant="standard" onChange={(event)=>{setPhone(event.target.value)}} />          
-            <TextField label="Username" variant="standard" onChange={(event)=>{setUser(event.target.value)}} />          
+            <Box style={inputStyle}>
+              <TextField style={inputWidth} label="Full Name" variant="standard" onChange={(event)=>{setName(event.target.value)}}/>          
+              <TextField style={inputWidth} label="Email Address" variant="standard" onChange={(event)=>{setEmail(event.target.value)}} />          
+              <TextField style={inputWidth} label="Phone Number" variant="standard" onChange={(event)=>{setPhone(event.target.value)}} />          
+              <TextField style={inputWidth} label="Username" variant="standard" onChange={(event)=>{setUser(event.target.value)}} />          
             </Box>
         </Box>     
         // break;
 
       case 2:
-        return <Box theme={theme}>
-                  <Typography>Skill Level</Typography>
-                  <Typography>Please tell us about your skill level in frontend development</Typography>
-                  <Box id="hello">
-                    <FormControl component="fieldset" name="method-of-payment">
-
-                    <RadioGroup row  aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group" onChange={(event)=>{setLevel(event.target.value)}} >
-                      <FormControlLabel value="beginner" control={<Radio />} label="Beginner" />
-                      <FormControlLabel value="intermediate" control={<Radio />} label="Intermediate" />
-                      <FormControlLabel value="advanced" control={<Radio />} label="Advanced" />
-                      <FormControlLabel value="other" control={<Radio />} label="Other" />
-                    </RadioGroup>  
-                    </FormControl>    
+        return <Box style={formStyle}>
+                  <Box>
+                    <Typography>Skill Level</Typography>
+                    <Typography>Please tell us about your skill level in frontend development</Typography>
+                  </Box>
+                  <Box>
+                      <RadioGroup style={inputStyle} row  aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group" onChange={(event)=>{setLevel(event.target.value)}} >
+                        <FormControlLabel style={inputWidth} value="beginner" control={<Radio />} label="Beginner" />
+                        <FormControlLabel style={inputWidth} value="intermediate" control={<Radio />} label="Intermediate" />
+                        <FormControlLabel style={inputWidth} value="advanced" control={<Radio />} label="Advanced" />
+                        <FormControlLabel style={inputWidth} value="other" control={<Radio />} label="Other" />
+                      </RadioGroup>  
                   </Box>
                </Box>     
         
@@ -104,15 +137,15 @@ function App() {
 
       case 3:
   
-        return <Box>
+        return <Box style={formStyle}>
                   <Typography >Challenges Preference</Typography>
                   <Typography>Please tell us which frontend challenges you would like to participated in</Typography>
                   <Box>
-                    <RadioGroup row  aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group" onChange={(event)=>{setPreference(event.target.value)}} >
-                      <FormControlLabel value="html/css/js" control={<Radio />} label="HTML/CSS/JS" />
-                      <FormControlLabel value="react" control={<Radio />} label="React.js" />
-                      <FormControlLabel value="vue" control={<Radio />} label="Vue.js" />
-                      <FormControlLabel value="other" control={<Radio />} label="Other" />
+                    <RadioGroup style={inputStyle} row  aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group" onChange={(event)=>{setPreference(event.target.value)}} >
+                      <FormControlLabel style={inputWidth} value="html/css/js" control={<Radio />} label="HTML/CSS/JS" />
+                      <FormControlLabel style={inputWidth} value="react" control={<Radio />} label="React.js" />
+                      <FormControlLabel style={inputWidth} value="vue" control={<Radio />} label="Vue.js" />
+                      <FormControlLabel style={inputWidth} value="other" control={<Radio />} label="Other" />
                     </RadioGroup>      
                   </Box>
                 </Box>     
@@ -120,28 +153,28 @@ function App() {
         // break;
 
       case 4:
-        return <Box>
-                  <Box>
+        return <Box style={reviewStyle} >
+                  <Box style={reviewBox}>
                     <Typography>Full Name</Typography>
                     <Typography>{name}</Typography>
                   </Box>
-                  <Box>
+                  <Box style={reviewBox}>
                     <Typography>Email Address</Typography>
                     <Typography>{email}</Typography>
                   </Box>
-                  <Box>
+                  <Box style={reviewBox}>
                     <Typography>Phone</Typography>
                     <Typography>{phone}</Typography>
                   </Box>
-                  <Box>
+                  <Box style={reviewBox}>
                     <Typography>Username</Typography>
                     <Typography>{user}</Typography>
                   </Box>
-                  <Box>
+                  <Box style={reviewBox}>
                     <Typography>Level</Typography>
                     <Typography>{level}</Typography>
                   </Box>
-                  <Box>
+                  <Box style={reviewBox}>
                     <Typography>Preference</Typography>
                     <Typography>{preference}</Typography>
                   </Box>
@@ -155,11 +188,13 @@ function App() {
   }
 
   return (
-    <Container>
+    <Container style={containerStyle}>
 
-      <Box sx={{ width: '100%' }} >
-        <Typography>Join Our Community Developer</Typography>
-        <Typography>To join our community and participate challenges, Please fill out the following </Typography>
+      <Box style={boxStyle} spacing={8}>
+        <Box>
+          <Typography>Join Our Community Developer</Typography>
+          <Typography>To join our community and participate challenges, Please fill out the following </Typography>
+        </Box>
         <Stepper activeStep={activeStep}>
           {steps.map((label, index) => {
             const stepProps = {};
@@ -187,9 +222,9 @@ function App() {
 
           <React.Fragment>
             <CssBaseline />
-            <ThemeProvider theme={container}>
+            <Box >
               {stepContent(activeStep+1)}
-            </ThemeProvider>
+            </Box>
             <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
               <Button
                 color="inherit"
